5411506b5301a6f2f1aee882ca531176708016fd
Vivek Kalyan <vivekkalyansk@gmail.com>
2018-01-02 12:55:32 +0800

    Upgrade version 0.3->0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='metascrapy',
-      version='0.3',
+      version='0.4',
       description='Scrapes meta data from a link with python',
       url='https://github.com/bowsplinter/metascrapy',
       author='Vivek Kalyan',
