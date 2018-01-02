c44f24e5b5d8f5706be44572116707d7aac82301
Vivek Kalyan <vivekkalyansk@gmail.com>
2018-01-02 12:54:16 +0800

    Refactor code with list of functions for fallbacks

--- metascrapy/Metadata.py
+++ metascrapy/Metadata.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import functools
+from functools import reduce
 
 class Metadata(object):
     """docstring for Metadata"""
@@ -29,47 +29,34 @@ class Metadata(object):
 
     def scrape(self, link):
         self.__init_values()
+
         result = requests.get(link)
-
-        try:
-            self.x_frame_options = result.headers['X-Frame-Options']
-        except:
-            pass
-
         soup = BeautifulSoup(result.content, "html.parser")
 
+        funcs_xframeoptions = [
+            lambda x: x.headers['X-Frame-Options']
+        ]
         funcs_title = [
             lambda x: x.find(property='og:title')['content'],
             lambda x: x.title.string
         ]
+        funcs_description = [
+            lambda x: x.find(property='og.description')['content'],
+            lambda x: x.find('p').string
+        ]
+        funcs_image = [
+            lambda x: x.find(property='og:image')['content']
+        ]
+        funcs_numwords = [
+            lambda x: reduce(lambda a,b: a+len(b.get_text().split()), x.find_all('p'), 0),
+        ]
+        funcs_lang = [
+            lambda x: x.find('html')['lang']
+        ]
+
+        self.x_frame_options = self.parse(result, funcs_xframeoptions)
         self.title = self.parse(soup, funcs_title)
-
-        soup_description = soup.find(property='og:description')
-        if soup_description:
-            self.description = soup_description['content']
-        else:
-            soup_first_paragraph = soup.find('p')
-            if soup_first_paragraph:
-                self.description = soup_first_paragraph.string
-            else:
-                self.description = None
-
-        soup_image = soup.find(property='og:image')
-        if soup_image:
-            self.image = soup_image['content']
-        else:
-            self.image = None
-
-        soup_paragraphs = soup.find_all("p",string=re.compile("\w+"))
-        self.num_words = 0
-        for sentence in soup_paragraphs:
-            self.num_words += len(sentence.string.split())
-
-        soup_html = soup.find('html')
-        if soup_html:
-            try:
-                self.lang = soup_html['lang']
-            except:
-                self.lang = None
-        else:
-            self.lang = None
+        self.description = self.parse(soup, funcs_description)
+        self.image = self.parse(soup, funcs_image)
+        self.num_words = self.parse(soup, funcs_numwords)
+        self.lang = self.parse(soup, funcs_lang)
